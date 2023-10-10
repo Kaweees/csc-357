87409faa381127180aa40f71cb1a81f4d3b8a117
Miguel Villa Floran <miguel.villafloran@gmail.com>
2023-10-09 20:48:14 -0700

    Make code Nico quality

--- asgns/asgn-01/detab.c
+++ asgns/asgn-01/detab.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
 
-#define TAB_WIDTH      8   // size of a tab
-#define TAB_CHAR       9   // asci code for tab
-#define BACKSPACE_CHAR 8   // asci code for a backspace
-#define NEWLINE_CHAR   10  // asci code for a newline
-#define CARRIAGE_CHAR  13  // asci code for a carriage return
-#define SPACE_CHAR     32  // asci code for a space
+#define TAB_WIDTH      8  /* size of a tab */
+#define TAB_CHAR       9  /* asci code for tab */
+#define BACKSPACE_CHAR 8  /* asci code for a backspace */
+#define NEWLINE_CHAR   10 /* asci code for a newline */
+#define CARRIAGE_CHAR  13 /* asci code for a carriage return */
+#define SPACE_CHAR     32 /* asci code for a space */
 
 int main(int argc, char *argv[]) {
-  int ch;         // used to store the current char from stdin
-  int count = 0;  // used to store amount of chars in current line
+  int ch;        /* used to store the current char from stdin */
+  int count = 0; /* used to store amount of chars in current line */
   while ((ch = getchar()) != EOF) {
     switch (ch) {
       case (TAB_CHAR): {
-        for (int i = count % TAB_WIDTH; i < TAB_WIDTH; i++) {
+        int i;
+        for (i = count % TAB_WIDTH; i < TAB_WIDTH; i++) {
           putchar(SPACE_CHAR);
-          count++;  // increment count for each space
+          count++; /* increment count for each space */
         }
         break;
       }
       case (NEWLINE_CHAR):
       case (CARRIAGE_CHAR): {
         putchar(ch);
-        count = 0;  // reset count on new line
+        count = 0; /* reset count on new line */
         break;
       }
       case (BACKSPACE_CHAR): {
         if (count) {
           putchar(ch);
-          count--;  // decrement count for each backspace only if count > 0
+          count--; /* decrement count for each backspace only if count > 0 */
         }
         break;
       }
