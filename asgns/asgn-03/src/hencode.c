5131834aeec3b400b95128f093778e2aee740e10
Miguel Villa Floran <miguel.villafloran@gmail.com>
2023-11-04 20:02:31 -0700

    Update hencode.c

--- asgns/asgn-03/src/hencode.c
+++ asgns/asgn-03/src/hencode.c
@@ -38,26 +38,32 @@ void hencode(int infile, int outfile) {
     int count = 0;
     uint32_t frequencyNetworkByte = 0;
     char* correspondingCode = NULL;
-    for (int i = 0; i < file_contents->file_size; i++) {
+    int i;
+    int j;
+    for (i = 0; i < file_contents->file_size; i++) {
       correspondingCode = huffman_codes[(int)file_contents->file_contents[i]];
-      for (int j = 0; j < strlen(correspondingCode); j++) {
+      printf("%c: ", file_contents->file_contents[i]);
+      for (j = 0; j < strlen(correspondingCode); j++) {
         if (correspondingCode[j] == '1') {
           frequencyNetworkByte |=
               (1 << ((sizeof(correspondingCode) * BITS_PER_BYTE) - 1 - count));
         }
+        printf("%c", correspondingCode[j]);
         count++;
         if (count == sizeof(uint32_t) * BITS_PER_BYTE) {
           frequencyNetworkByte = htonl(frequencyNetworkByte);
           safe_write(outfile, &frequencyNetworkByte, sizeof(uint32_t));
+          printf(" - ");
           frequencyNetworkByte = 0;
           count = 0;
         }
       }
     }
+    printf("\n");
     /* Change the byte order to network byte order (big endian) */
     frequencyNetworkByte = htonl(frequencyNetworkByte);
     /* Write the byte to the file if the byte is full */
-    safe_write(outfile, &frequencyNetworkByte, count / BITS_PER_BYTE);
+    safe_write(outfile, &frequencyNetworkByte, (count / BITS_PER_BYTE) + (count % BITS_PER_BYTE));
     freeFileContent(file_contents);
     freeFrequencyList(char_freq);
     freeHuffmanTree(root);           /* Free the Huffman tree */
