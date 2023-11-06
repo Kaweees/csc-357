fe1b528449136cfe543cb403d9beb05676d4a0f1
Miguel Villa Floran <miguel.villafloran@gmail.com>
2023-11-05 16:44:14 -0800

    Update safe dir

--- labs/lab-05/src/safe_dir.c
+++ labs/lab-05/src/safe_dir.c
@@ -5,6 +5,14 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+/**
+ * A safe version of opendir that validates the directory stream and exits on failure
+ *
+ * @param filename The name of the directory to open.
+ * @param flags The flags to use when opening the directory.
+ * @param mode The mode to use when opening the directory.
+ * @return A pointer to the directory stream.
+ */
 DIR safeOpenDir(char *filename, int flags, mode_t mode) {
   DIR *dir;
   /* Open the directory */
@@ -25,6 +33,12 @@ DIR safeOpenDir(char *filename, int flags, mode_t mode) {
   }
 }
 
+/**
+ * A safe version of readdir that validates the directory contents and exits on failure
+ *
+ * @param dir The directory stream to read from.
+ * @return A pointer to the directory contents.
+ */
 DirContent *safeReadDir(DIR *dir) {
   struct dirent *entry DirContent *dir_contents =
       safeMalloc(sizeof(DirContent));
@@ -35,13 +49,43 @@ DirContent *safeReadDir(DIR *dir) {
   return dir_contents;
 }
 
+/**
+ * A safe version of rewinddir that validates the rewinded stream and exits on failure
+ *
+ * @param fd The directory stream to rewind.
+ */
 void safeRewindDir(int fd) {}
-void safeCloseDir(int fd) {}
-void freeDirContent(DirContent *dir_contents) {}
 
-// opendir(3) open a directory for reading
-// readdir(3) read a directory entry
-// rewinddir(3) rewind a directory to the beginning
-// closedir(3) close a directory
-// stat(2) get file status
-// lstat(2) get file status
+/**
+ * A safe version of closedir that validates the closed stream and exits on failure
+ *
+ * @param fd The directory stream to rewind.
+ */
+void safeCloseDir(int fd) {}
+
+
+/**
+ * A safe version of stat that validates the file status (symlinks are followed) and exits on failure
+ *
+ * @param path The path to the file to stat.
+  * @param buf The buffer to store the file status in.
+ */
+void safeStat(char *path, struct stat *buf) {
+  if (stat(path, buf) == -1) {
+    perror("Failed to stat file");
+    exit(EXIT_FAILURE);
+  }
+}
+
+/**
+ * A safe version of lsat that validates the file status (symlinks aren't followed) and exits on failure
+ *
+ * @param path The path to the file to stat.
+ * @param buf The buffer to store the file status in.
+ */
+void safeLstat(char *path, struct stat *buf) {
+  if (lstat(path, buf) == -1) {
+    perror("Failed to stat file");
+    exit(EXIT_FAILURE);
+  }
+}
